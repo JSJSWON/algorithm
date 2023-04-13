a8d86414c97e26902f48a5fa70edc1d17bb59541
Junsik Won <cowdog10@naver.com>
2023-04-13 15:47:39 +0900

    add

--- leetcode/medium/39.py
+++ leetcode/medium/39.py
@@ -12,7 +12,8 @@ class Solution:
                 result.append(path)
                 return  # 탐색 종료(가지 치기)
             for i in range(index, len(candidates)):
-                # dfs 함수에 i가 들어감으로서 자기 자신을 포함해 뒤에 있는 숫자들을 탐색할 수 있음
+                # dfs 함수에 i가 들어감으로써 자기 자신을 포함해 뒤에 있는 숫자들을 탐색할 수 있음
+                # 이로 인해 candidates[i] == 0이면 무한히 깊이 탐색을 하게 됨(테스트 케이스에는 0이 없지만)
                 dfs(csum - candidates[i], i, path + [candidates[i]])
 
         dfs(target, 0, [])
